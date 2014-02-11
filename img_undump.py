32ef01d1de9eab82bc5e86a1f182b2d51ce992e2
William O'Hanley <willy.ohanley@gmail.com>
2014-02-11 00:17:20 -0500

    rebuild image

--- img_undump.py
+++ img_undump.py
@@ -9,3 +9,4 @@ data = open(args.source + '.dump', 'rb').read()
 meta = open(args.source + '.meta').readlines()
 
 output = Image.frombytes('RGB', (int(meta[0]), int(meta[1])), data)
+output.save(args.source)
